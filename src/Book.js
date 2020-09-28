dcd0deeaa0c4279d3bc82e3c463e803307469937
Romulo Costa <rgcostabox@gmail.com>
2020-09-27 21:46:30 -0400

    Segregate app into components Dashboard, Bookshelf, Book and SearchBooks. Also, added routing to the app.

---
+++ src/Book.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Book extends Component {
+
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.url})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{ this.props.title }</div>
+                <div className="book-authors">{ this.props.author }</div>
+            </div>
+        );
+    }
+}
+
+Book.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    url: PropTypes.string
+}
+
+export default Book;
